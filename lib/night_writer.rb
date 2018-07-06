a7baea9a1fdc62e0d750622f8869682e888d021c
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-07-06 17:48:10 -0600

    completes iteration 1

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,8 +1,23 @@
+require 'pry'
+
 class NightWriter
 
 ARGV == ["string_1", "string_2"]
 ARGV[0] == "string_1"
 ARGV[1] == "string_2"
 
-puts "Created '#{ARGV[1]}' containing 256 characters."
+message_reader = File.open(ARGV[0], "r")
+incoming_text = message_reader.read
+
+word_count = incoming_text.split.count
+message_reader.close
+
+capitalized_text = incoming_text.upcase
+
+message_writer = File.open(ARGV[1], "w")
+message_writer.write(capitalized_text)
+
+puts word_count
+puts "Created '#{ARGV[1]}' containing #{word_count} characters."
+
 end
