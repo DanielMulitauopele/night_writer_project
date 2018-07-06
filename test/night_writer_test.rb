a81f1695d29a5cf1cb1c2e74bdbc53f338f3ea30
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-07-06 16:53:06 -0600

    adds instance test

--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -1,8 +1,17 @@
-require "minitest/autorun"
-require
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/night_writer.rb'
 
-# adding a new branch
-class NightWriterTest
-aksudbaskjdnaskdn
-kjsndckjsndckjsdn
+class NightWriterTest < Minitest::Test
+  def test_it_exists
+    night_writer = NightWriter.new
+
+    assert_instance_of NightWriter, night_writer
+  end
+
+  def test_it_can_receive_inputs
+    night_writer = NightWriter.new
+
+    # assert_instance_of NightWriter, night_writer
+  end
 end
