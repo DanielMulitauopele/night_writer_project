c2619e9ca51cb09f5a1914655a4d358b79d5e561
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-07-07 15:19:44 -0600

    small changes

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,24 +1,28 @@
 require './lib/braille_translator.rb'
-
+require 'pry'
 class NightWriter
 
-ARGV == ["string_1", "string_2"]
-ARGV[0] == ("string_1")
-ARGV[1] == ("string_2")
+# This receives inputs from the command line
+ARGV == ["input_1", "output_1"]
+ARGV[0] == ("input_1")
+ARGV[1] == ("output_1")
 
+# This reads the file that ARGV[0] recieved
 message = File.open(ARGV[0], "r")
-speech_string = message.read
-characters = speech_string.chars
+message_text = message.read
+
+# This is where we are doing actions to the text that we read
+characters = message_text.chars
 character_count = (characters.count - 1)
 
 braille_translator = BrailleTranslator.new
-binary_translation = braille_translator.translate(speech_string)
+braille_translation = braille_translator.translate(message_text)
 
 message.close
 
-
+# This is where we paste all the conversions we've done so far
 braille = File.open(ARGV[1], "w")
-braille.write(binary_translation)
+braille.write(braille_translation)
 
 braille.close
 
