e31ce3813099d8527fb2586f0ecdcc6b3448e22a
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-07-07 12:58:39 -0600

    adds complete translator

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -9,13 +9,16 @@ ARGV[1] == ("string_2")
 message = File.open(ARGV[0], "r")
 speech_string = message.read
 characters = speech_string.chars
-character_count = characters.count
+character_count = (characters.count - 1)
 
+braille_translator = BrailleTranslator.new
+binary_translation = braille_translator.translate(speech_string)
 
 message.close
 
+
 braille = File.open(ARGV[1], "w")
-braille.write(character_count)
+braille.write(binary_translation)
 
 braille.close
 
