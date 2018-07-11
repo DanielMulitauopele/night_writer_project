9787da62d719b86ea51c0368c6908cc46a2a21eb
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-07-11 12:51:43 -0600

    finalizes braille tests

--- test/braille_translator_test.rb
+++ test/braille_translator_test.rb
@@ -15,6 +15,7 @@ def test_it_turns_input_into_chars
   braille_translator = BrailleTranslator.new
   expected = ["c", "a", "t"]
   actual = braille_translator.turns_input_into_chars("cat")
+
   assert_equal expected, actual
 end
 
@@ -23,6 +24,7 @@ def test_it_can_convert_one_letter
   input_array = braille_translator.turns_input_into_chars("a")
   expected = [["0.", "..", ".."]]
   actual = braille_translator.convert_chars_into_braille(input_array)
+
   assert_equal expected, actual
 end
 
@@ -31,6 +33,7 @@ def test_it_can_convert_more_than_one_letter
   input_array = braille_translator.turns_input_into_chars("ab")
   expected = [["0.", "..", ".."], ["0.", "0.", ".."]]
   actual = braille_translator.convert_chars_into_braille(input_array)
+
   assert_equal expected, actual
 end
 
@@ -40,6 +43,7 @@ def test_it_deletes_nil_from_array
   braille_letters = braille_translator.convert_chars_into_braille(input_array)
   expected = [["0.", "..", ".."], ["0.", "0.", ".."]]
   actual = braille_translator.deletes_nil_from_array(braille_letters)
+
   assert_equal expected, actual
 end
 
@@ -50,24 +54,34 @@ def test_it_can_transpose_one_letter
   braille_letters_nilless = braille_translator.deletes_nil_from_array(braille_letters)
   expected = [["0."], [".."], [".."]]
   actual = braille_letters_nilless.transpose
+
   assert_equal expected, actual
 end
 
 def test_it_can_transpose_more_than_one_letter
   braille_translator = BrailleTranslator.new
-  input_array = braille_translator.turns_input_into_chars("a")
+  input_array = braille_translator.turns_input_into_chars("ab")
   braille_letters = braille_translator.convert_chars_into_braille(input_array)
   braille_letters_nilless = braille_translator.deletes_nil_from_array(braille_letters)
-  expected = [["0."], [".."], [".."]]
+  expected = [["0.", "0."], ["..", "0."], ["..", ".."]]
   actual = braille_letters_nilless.transpose
+
   assert_equal expected, actual
 end
 
 def test_it_can_translate_one_letter
-  skip
   braille_translator = BrailleTranslator.new
-  expected = ["0.", "..", ".."]
+  expected = [["0."], [".."], [".."]]
   actual = braille_translator.translate("a")
+
+  assert_equal expected, actual
+end
+
+def test_it_can_translate_a_word
+  braille_translator = BrailleTranslator.new
+  expected = [["0.0.0.0.0."], ["00.00.0..0"], ["....0.0.0."]]
+  actual = braille_translator.translate("hello")
+
   assert_equal expected, actual
 end
 
