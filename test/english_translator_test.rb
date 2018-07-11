b0e908d52e784d8dfbc10c99fe7dbfc120e72870
Averi Johnson <averimj@live.com>
2018-07-11 16:45:23 -0600

    Add english translator test

---
+++ test/english_translator_test.rb
@@ -0,0 +1,25 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/english_translator.rb"
+
+class EnglishTranslatorTest < Minitest::Test
+
+  def test_it_exists
+    english_translator = EnglishTranslator.new
+
+    assert_instance_of EnglishTranslator, english_translator
+  end
+
+  def method_name
+
+  end
+
+
+
+
+
+
+
+
+
+end
