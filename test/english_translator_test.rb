9a379a66156e9ea565e8b3fe82ba319868ade7ac
Averi Johnson <averimj@live.com>
2018-07-11 18:27:44 -0600

    add structure for test

--- test/english_translator_test.rb
+++ test/english_translator_test.rb
@@ -10,16 +10,40 @@ class EnglishTranslatorTest < Minitest::Test
     assert_instance_of EnglishTranslator, english_translator
   end
 
-  def method_name
+  def test_it_can_break_braille_into_chunks
+    english_translator = EnglishTranslator.new
 
+    assert_equal ""
   end
 
+  def test_it_can_break_strings_into_smaller_strings
+    english_translator = EnglishTranslator.new
 
+    assert_equal ""
+  end
 
+  def test_it_can_zip_elements
+    english_translator = EnglishTranslator.new
 
+    assert_equal ""
+  end
 
+  def test_elements_are_in_braille
+    english_translator = EnglishTranslator.new
 
+    assert_equal ""
+  end
 
+  def test_braille_is_converted
+    english_translator = EnglishTranslator.new
 
+    assert_equal ""
+  end
+
+  def test_the_characters_are_joined
+    english_translator = EnglishTranslator.new
+
+    assert_equal ""
+  end
 
 end
